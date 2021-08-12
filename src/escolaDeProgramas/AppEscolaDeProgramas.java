6bda5a232f49227e8dfe554bc43f4ca2f35af68e
salocinrevenge <80754944+salocinrevenge@users.noreply.github.com>
2021-08-12 19:40:12 -0300

    primeiras classes

---
+++ src/escolaDeProgramas/AppEscolaDeProgramas.java
@@ -0,0 +1,10 @@
+package escolaDeProgramas;
+
+public class AppEscolaDeProgramas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/escolaDeProgramas/Escola.java
@@ -0,0 +1,5 @@
+package escolaDeProgramas;
+
+public class Escola {
+
+}
