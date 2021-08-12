4bc52150a5046265cdb0fa9ccf947dca0c2b538b
salocinrevenge <80754944+salocinrevenge@users.noreply.github.com>
2021-08-12 19:45:39 -0300

    testando o push

--- src/escolaDeProgramas/Escola.java
+++ src/escolaDeProgramas/Escola.java
@@ -2,4 +2,6 @@ package escolaDeProgramas;
 
 public class Escola {
 // essa classe deve conter a escola e todos os seus alunos, alem de seu funcionamento
+	
+	//possuir a funcao tick e a funcao render
 }
