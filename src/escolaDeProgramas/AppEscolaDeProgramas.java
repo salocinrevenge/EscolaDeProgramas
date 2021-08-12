063c14e510779724183a2e32622834572ce475c1
MarryAngel <70298868+MarryAngel@users.noreply.github.com>
2021-08-12 20:25:01 -0300

    Começando window

--- src/escolaDeProgramas/AppEscolaDeProgramas.java
+++ src/escolaDeProgramas/AppEscolaDeProgramas.java
@@ -4,7 +4,7 @@ public class AppEscolaDeProgramas {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		new Escola();
 	}
 
 }
--- src/escolaDeProgramas/Window.java
+++ src/escolaDeProgramas/Window.java
@@ -1,5 +1,20 @@
 package escolaDeProgramas;
 
-public class Window {
-//teste
+import java.awt.Dimension;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Window extends JFrame{
+	private static final long serialVersionUID = 1L;
+	
+	public Window() {
+		super("Escola de Programas");
+		this.setPreferredSize(new Dimension(1000,500));
+		this.setResizable(false);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		//add(desenho);                           
+		pack();				       	    
+		setVisible(true);
+	}
+	
 }
