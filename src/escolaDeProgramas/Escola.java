2cf0bdf82dfe27da50143ae0850ab68abb2ce610
salocinrevenge <80754944+salocinrevenge@users.noreply.github.com>
2021-08-12 20:03:42 -0300

    divisao de trabalhos inicial 2

--- src/escolaDeProgramas/Escola.java
+++ src/escolaDeProgramas/Escola.java
@@ -2,7 +2,10 @@ package escolaDeProgramas;
 
 public class Escola {
 // essa classe deve conter a escola e todos os seus alunos, alem de seu funcionamento
-	
+	Window janela;
 	//possuir a funcao tick e a funcao render
-	//yugoslaia
+	public Escola()
+	{
+		this.janela = new Window();
+	}
 }
